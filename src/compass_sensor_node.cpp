11f65b8d6d276be36598f7cf6a9571e18d18fce8
reyk <reyk-carstens@web.de>
2022-06-02 15:53:30 +0200

    changes

--- src/compass_sensor_node.cpp
+++ src/compass_sensor_node.cpp
@@ -17,11 +17,14 @@ using namespace std;
 CompassSensorNode::CompassSensorNode(): BaseSensorNode("compass") {
     this->publisher = this->create_publisher<std_msgs::msg::Float32>("compass", 10);
 
-    this->bp = BrickPi3(0x02);
-    this->bp.set_sensor_type(this->port, SENSOR_TYPE_I2C);
-
     this->sensor.address = 0x42;
+    this->sensor.speed = 20;
+    this->sensor.length_write = 1;
+    this->sensor.buffer_write[0] = 0x42;
     this->sensor.length_read = 4;
+
+    this->bp = BrickPi3();
+    this->bp.set_sensor_type(this->port, SENSOR_TYPE_I2C, 0x0, &this->sensor);
 }
 
 void CompassSensorNode::publish_value_callback() {
@@ -36,10 +39,13 @@ int CompassSensorNode::read_value_from_brickpi3() {
 }
 
 float CompassSensorNode::get_angle_from_read_buffer() {
+    this->read_value_from_brickpi3();
+    this->bp.get_sensor(this->port, &this->sensor);
+
     int value = (
-        this->sensor.buffer_read[3] << 24 
-        + this->sensor.buffer_read[2] << 16
-        + this->sensor.buffer_read[1] << 8
+        (this->sensor.buffer_read[3] << 24)
+        + (this->sensor.buffer_read[2] << 16)
+        + (this->sensor.buffer_read[1] << 8)
         + this->sensor.buffer_read[0]
     );
 
